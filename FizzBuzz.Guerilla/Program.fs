08e2a354bd434e362c7f9e8eef83d26a945525e7
Fabiano Teodoro <faateodoro@gmail.com>
2022-05-23 23:24:14 -0300

    código em apenas uma função

--- FizzBuzz.Guerilla/Program.fs
+++ FizzBuzz.Guerilla/Program.fs
@@ -1,2 +1,22 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "Please enter a valid integer between 1 and 4000:"
+    let input = Console.ReadLine()
+    match Int32.TryParse input with
+    | false,_ -> printfn $"{input} is not a valid integer."
+    | true, number ->
+        match 1 <= number && number <= 4000 with
+        | false -> printfn $"You entered {number}. Please enter a number between 1 and 4000."
+        | true -> 
+            [1 .. number]
+            |> List.map (fun n -> (n, n % 3, n % 5))
+            |> List.map (function
+                | (_,0,0) -> "FizzBuzz"
+                | (_,0,_) -> "Fizz"
+                | (_,_,0) -> "Buzz"
+                | (n,_,_) -> string n)
+            |> String.concat "\n"
+            |> printfn "Here is the output:\n%s"
+    0
